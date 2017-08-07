36b60aaecdb38d2a02d7126d14c598b915f33b6a
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-08 00:18:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get("/article-one", function (req, res){
-  res.send("Article one requested and will be served");  
+  res.send('Article one requested and will be served');  
 });
 
 app.get("/article-two", function (req, res){
-  res.send("Article two requested and will be served");  
+  res.send('Article two requested and will be served');  
 });
 
 app.get("/article-three", function (req, res){
-  res.send("Article tree requested and will be served");  
+  res.send('Article tree requested and will be served');  
 });
 
 app.get('/ui/style.css', function (req, res) {
