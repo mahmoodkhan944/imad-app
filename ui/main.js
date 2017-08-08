661bc4e646048b5ac6a50c1ec3b133c5635c8130
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-09 02:47:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML = 'New value';
 // Move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
-  img.style.margineLeft = '100px';  
+  img.style.marginLeft = '100px';  
 };
