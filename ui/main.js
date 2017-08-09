8cf8fd011cb7b7ef1427ac620b1bf2dea75283ed
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-09 06:16:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,21 +57,3 @@ submit.onclick = function () {
   request.send(null);  
 
 };
-
-console.log('Loaded!');
-
-//Change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'list';
-
-// Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
-};
