695b6247b12b334a488cec9c888cbb0426abeb3f
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-09 06:06:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,15 @@ button.onclick =function () {
 // Submit name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-
+    
+// Create a request object
+  var request = new XMLHttpRequest();
+  
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200){
               // Capture a list of names and render it as a list
               var names = request.responseText;
               names = JSON.parse(names);
@@ -37,6 +45,8 @@ submit.onclick = function () {
               }
               var ul = document.getElementById('namelist');
               ul.innerHTML = list;
+          }
+      }
       // Not done yet
   };
   
