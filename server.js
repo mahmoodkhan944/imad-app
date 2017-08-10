20ad581d401112d8c73a71eba277baa32efbbf19
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-10 21:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db', function (req, res) {
+   // make a select request
+   // return a response with the results
+ });
+
 var counter = 0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
