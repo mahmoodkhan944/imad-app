be1490c0ea4f31c9cbb40f27b100fbcca17733d1
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-10 23:02:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,9 +121,22 @@ app.get('/submit-name', function(req, res) { // URL: /submt-name?name=xxxxx
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+    //articleName === article-one
+    //articles[articleName] == {} content object for article one
+    
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+               res.status(404).send('Article not found');
+           } else {
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
