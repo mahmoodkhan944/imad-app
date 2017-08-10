373668131b4d7200b7030464cc7fe9e50a75899b
mahmoodkhan944 <30691897+mahmoodkhan944@users.noreply.github.com>
2017-08-10 22:19:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'mahmoodkhan944',
+    database: 'mahmoodkhan944',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -83,9 +92,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
    // make a select request
    // return a response with the results
+   pool.query('SELECT * FROM test', function (err, result){
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+   });
  });
 
 var counter = 0;
